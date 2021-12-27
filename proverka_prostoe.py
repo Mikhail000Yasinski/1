87e38454ce224015edffabce63ab9f01fc8dcedc
Mikhail000Yasinski <90722893+Mikhail000Yasinski@users.noreply.github.com>
2021-12-28 00:18:28 +0300

    Update proverka_prostoe.py

--- proverka_prostoe.py
+++ proverka_prostoe.py
@@ -1,21 +1,101 @@
+import unittest
+from sympy.ntheory import primetest, factorint
+import math
+
+def is_prime(num):
+    result = True
+    i = 2
+    if num >= 2:
+        r = int(num**0.5)+ 1
+        for i in range (i, r):
+            if num % i == 0:
+                result = False
+                break
+            i += 1
+    else:
+        result = False    
+    return result
+  
+
+class PrimeCheckTest(unittest.TestCase):
+    """Тесты для функции проверки на простоту"""
+
+    def setUp(self):
+        """Инициализация"""
+        pass
+
+    @staticmethod
+    def is_exatly_prime_64(num):
+        """
+        Точная проверка того, простое ли (True) или составное (False) число,
+        работает только в диапазоне [0, 2*64)
+        """
+        if num >= 2 ** 64:
+            raise ValueError("Эта функция работает только в диапазоне [0, 2*64)")
+        return primetest.isprime(num)
+
+    def test_return_type(self):
+        """Проверка того, что функция возвращает bool"""
+        for i in range(-5, 10):
+            self.assertIsInstance(
+                is_prime(i), bool,
+                msg="Функция должна возвращать bool"
+            )
+
+    def test_nonned_1000(self):
+        """Проверка [0..999)"""
+        for i in range(1000):
+            self.assertEqual(
+                is_prime(i), PrimeCheckTest.is_exatly_prime_64(i),
+                msg=f"Функция наврала на небольшом положительном {i}"
+            )
+
+    def test_negatives_10(self):
+        """Проверка [-10, 0)"""
+        for i in range(-10, 0):
+            self.assertEqual(
+                is_prime(i), PrimeCheckTest.is_exatly_prime_64(i),
+                msg=f"Функция наврала на отрицательном {i}"
+            )
+
+    def test_carmichael(self):
+        """Проверка на числах Кармайкла"""
+        # можно было sympy.functions.combinatorial.numbers.carmichael,
+        # но очень не быстро, лучше взять готовые https://oeis.org/A002997
+        carmichaels = [
+            561, 1105, 1729, 2465, 2821, 6601, 8911, 10585, 15841, 29341,
+            41041, 46657, 52633, 62745, 63973, 75361, 101101, 115921, 126217,
+            162401, 172081, 188461, 252601, 278545, 294409, 314821, 334153,
+            340561, 399001, 410041, 449065, 488881, 512461
+        ]
+
+        for _ in range(10000):
+            for c in carmichaels:
+                self.assertFalse(
+                    is_prime(c),
+                    msg=f"Функция наврала на числе Кармайкла {c}"
+                )
+
+    def test_trivial_pseudorandoms(self):
+        """
+        Проверка на тривиальных псевдослучайных
+        Генерация линейным конгруэнтным генератором
+        https://en.wikipedia.org/wiki/Linear_congruential_generator#Parameters_in_common_use
+        в память о ZX81
+        """
+        a = 75
+        s = 1
+        c = 74
+        m = 1 << 16 + 1
+        for _ in range(50):
+            s = (s * a + c) % m
+            r = s % ( 1 << 16 )  # 16 бит
+            self.assertEqual(
+                is_prime(r), PrimeCheckTest.is_exatly_prime_64(r),
+                msg=f"Функция наврала на {r}"
+            )
+
+if __name__ == '__main__':
+    unittest.main()
 
 
-s_p = [2]  # добавим двойку как очевидное первое простое число, это пригодится далее
-n = 1949340  # число, до которого будем искать простые
-for i in range(3, n + 1, 2): # перебираем числа, начиная с трёх и только чётные - двойка пригодилась
-    m = 0  # задаём переменную для числа делителей нацело
-    r = int(n**0.5) + 2  # достаточно перебирать только числа до корня из проверяемого, т.к остальные числа слишком близки к нему, чтобы не быть взаимно простыми - оптимизация!
-    for j in s_p:   
-        if j > r:             
-            break
-        if i % j == 0:  # проверяем делимость на простое число, которое уже попало в список
-            m = 1  # если делится хотя бы на одно из списка, значит число не является простым
-            break
-    if m == 0:  # если делимости на простое из списка не обнаружилось, добавляем число в список
-        s_p.append(i)
-
-p = s_p[-1]  # Последнее число в списке простых будет либо n либо не n, это и проверим
-if p == n:  
-    print ("Простое!")
-else:
-    print ("Не простое!")
